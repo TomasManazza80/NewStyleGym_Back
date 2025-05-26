453f80cdd05eeb556b06abcead3ffa42390ecf3e
TomasManazza80 <tomas.manazza19@gmail.com>
2025-05-25 22:07:35 -0300

    back

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var vexor = require("vexor");
 const productBought = require("./routes/productBoughtRoute");
 const activityPricesRoutes = require('./routes/activityPrices');
 const { Vexor } = vexor;
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://newstylegym-back.onrender.com';
 
 var app = express();  
 const PORT = 3000;
--- dbconfig.js
+++ dbconfig.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
 
 module.exports = {
-  "db": {
-    "database": process.env.DATABASE,
-    "username": process.env.USERNAME,
-    "password": process.env.PASSWORD,
-    "host": process.env.HOST,
-    "port": parseInt(process.env.PORT), 
-    "dialect": process.env.DIALECT
-  },
+  db: {
+    database: process.env.DATABASE_NAME || process.env.DATABASE,
+    username: process.env.DB_USER || process.env.USERNAME,
+    password: process.env.DB_PASSWORD || process.env.PASSWORD,
+    host: process.env.DB_HOST || process.env.HOST,
+    port: parseInt(process.env.DB_PORT || 5432), // Puerto específico para DB
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: { // Configuración SSL obligatoria para Render
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    protocol: 'postgres',
+    logging: false // Desactiva logs de SQL en producción
+  }
 };
