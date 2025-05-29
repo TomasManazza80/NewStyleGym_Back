1962919efd5183375e78735217d74291f87dcfa7
Tomás Augusto Manazza <161159185+TomasManazza80@users.noreply.github.com>
2025-05-28 23:36:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const vexorInstance = new Vexor({
   apiKey: process.env.VEXOR_API_KEY,
 });
 
+app.use(express.json());
 // View engine setup
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
